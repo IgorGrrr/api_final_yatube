cff8eaa8f340196ed1ff7e369040678be9bfaabc
Игорь Дягилев <igordagilev@MacBook-Air-Igor.local>
2022-09-06 21:19:38 +0300

    версия для сдачи на ревью

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,58 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Follow, Group, User
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = serializers.SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
         fields = '__all__'
         model = Post
 
 
+class GroupSerializer(serializers.ModelSerializer):
+ 
+    class Meta:
+        fields = '__all__'
+        model = Group
+
+
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        slug_field='username',
+        read_only=True, 
     )
 
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        default=serializers.CurrentUserDefault(),
+        slug_field='username',
+        queryset=User.objects.filter()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        required=True,
+        queryset=User.objects.filter()
+    )
+
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following',)
+        validators = [UniqueTogetherValidator(
+            queryset=Follow.objects.all(),
+            fields=('user', 'following'),
+            message="You're already following this person."
+        )]
+
+    def validate(self, data):
+        if data['user'] == data['following']:
+            raise serializers.ValidationError("You can't follow yourself.")
+        return data
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,21 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import  DefaultRouter
+
+from .views import PostViewset, GroupViewset, CommentViewset, FollowViewset
+
+
+router = DefaultRouter()
+router.register('posts', PostViewset, basename='posts')
+router.register('groups', GroupViewset, basename='groups')
+router.register(
+    r'^posts/(?P<post_id>\d+)/comments',
+    CommentViewset,
+    basename='comments'
+)
+router.register('follow', FollowViewset, basename='follows')
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls.jwt'))
 ]
