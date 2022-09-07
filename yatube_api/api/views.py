e69102d141e404cf8b20213278847c991bd6cd23
Игорь Дягилев <igordagilev@MacBook-Air-Igor.local>
2022-09-07 23:30:27 +0300

    Исправление замечаний после ревью ч1

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -26,7 +26,6 @@ class GroupViewset(viewsets.ReadOnlyModelViewSet):
 
 
 class CommentViewset(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthorOrReadOnly]
 
@@ -46,12 +45,10 @@ class FollowViewset(mixins.ListModelMixin,
     serializer_class = FollowSerializer
     permission_classes = [permissions.IsAuthenticated]
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
-    search_fields = ('following__username',)
+    search_fields = ('following__username', 'user__username')
 
     def get_queryset(self):
-        user = self.request.user
-        follow = user.follower.all()
-        return follow
+        return self.request.user.follower.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
-    'djoser',
+    'simple_jwt',
     'api',
     'posts',
 ]
